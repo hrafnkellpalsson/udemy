a75d030d63fc7a089d5dbcb37250162b22a6cb02
Hrafnkell Palsson <hrafnkell.palsson@gmail.com>
2017-08-11 15:38:35 -0700

    Worked on axios and react-redux to make an API request

---
+++ react-redux/ReduxSimpleStarter-Section5/src/actions/index.js
@@ -0,0 +1,15 @@
+import axios from 'axios';
+
+const API_KEY = 'bf2871d3b9a0d0c46a5d1465b9fe2087';
+export const FETCH_WEATHER = 'FETCH_WEATHER';
+const ROOT_URL = `http://api.openweathermap.org/data/2.5/forecast?appid=${API_KEY}`;
+
+export function fetchWeather(city) {
+  const url = `${ROOT_URL}&q=${city},us`;
+  const request = axios.get(url);  
+
+  return {
+    type: FETCH_WEATHER,
+    payload: request,
+  };
+}
