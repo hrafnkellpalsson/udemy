5cd496079bd185825047f07607160359033eacc5
Hrafnkell Palsson <hrafnkell.palsson@gmail.com>
2017-08-11 17:12:11 -0700

    More work in advanced Redux portion in course

--- react-redux/ReduxSimpleStarter-Section5/src/reducers/reducer_weather.js
+++ react-redux/ReduxSimpleStarter-Section5/src/reducers/reducer_weather.js
@@ -1,3 +1,12 @@
-export default function(state=null, action) {  
+import { FETCH_WEATHER } from '../actions/index';
+
+export default function(state=[], action) {
+  switch (action.type) {
+    case FETCH_WEATHER:
+      // return state.push(action.payload.data); // BAD!!! Do not mutate state, we must always create a new fresh state
+      // return [action.payload.data].concat(state); // This works since concat doesn't mutate the array it operates on, rather it
+      // returns a new array. But we can use new fancy ES6 syntax...see below :)
+      return [ action.payload.data, ...state];
+  }
   return state;
 }
