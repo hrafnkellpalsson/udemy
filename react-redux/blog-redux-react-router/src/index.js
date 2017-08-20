58d91b468f33616616c737bae8a0d5d61fede862
Hrafnkell Palsson <hrafnkell.palsson@gmail.com>
2017-08-20 15:43:06 -0700

    Worked on blog.

--- react-redux/blog-redux-react-router/src/index.js
+++ react-redux/blog-redux-react-router/src/index.js
@@ -2,7 +2,7 @@ import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 import { createStore, applyMiddleware } from 'redux';
-import { BrowserRouter, Route } from 'react-router-dom';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import promise from 'redux-promise';
 
 // When we use React Router we don't need a single central component any more.
@@ -10,6 +10,7 @@ import promise from 'redux-promise';
 // import App from './components/app';
 import reducers from './reducers';
 import PostsIndex from './components/posts_index';
+import PostsNew from './components/posts_new';
 
 const createStoreWithMiddleware = applyMiddleware(promise)(createStore);
 
@@ -17,7 +18,10 @@ ReactDOM.render(
   <Provider store={createStoreWithMiddleware(reducers)}>
     <BrowserRouter>
       <div>
-        <Route path="/" component={PostsIndex} />
+        <Switch>
+          <Route path="/posts/new" component={PostsNew} />
+          <Route path="/" component={PostsIndex} />        
+        </Switch>
       </div>
     </BrowserRouter>
   </Provider>
