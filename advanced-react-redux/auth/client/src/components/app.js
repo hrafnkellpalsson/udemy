1c3611c75bf4eed04f656a9e3bb2d51388f1aa69
Hrafnkell Palsson <hrafnkell.palsson@gmail.com>
2017-09-30 16:35:11 -0700

    Started working on protected routes in the authentication portion of the advanced react/redux course and found out we're making use of a higher order component created earlier in its own section. So backtracked and just started working on that section.

--- advanced-react-redux/auth/client/src/components/app.js
+++ advanced-react-redux/auth/client/src/components/app.js
@@ -4,6 +4,7 @@ import Header from './header'
 import Signin from './auth/signin'
 import Signout from './auth/signout'
 import Signup from './auth/signup'
+import Feature from './feature'
 
 export default class App extends Component {
   render() {
@@ -14,6 +15,7 @@ export default class App extends Component {
         <Route path="/signin" component={Signin} />
         <Route path="/signout" component={Signout} />
         <Route path="/signup" component={Signup} />
+        <Route path="/feature" component={Feature} />
       </div>
     );
   }
